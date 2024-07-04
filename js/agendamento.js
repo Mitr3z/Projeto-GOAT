06916299a9d1d207b2be77a3bd426480611a9efb
Mitr3z <130659279+Mitr3z@users.noreply.github.com>
2024-07-04 18:48:56 -0300

    Delete js directory

--- js/agendamento.js
+++
@@ -1,6 +0,0 @@
-const barbearia = document.getElementById("barber-select")
-const servico = document.getElementById("service-select");
-const data = document.getElementById("dataSelecione");
-const hora = document.getElementById("horaSelecione");
-const nome = document.getElementById("contact-full-name");
-const telefone = document.getElementById("telefone");
