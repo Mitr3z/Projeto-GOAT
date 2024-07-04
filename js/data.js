66d4234ce32b570777c5799f58ffe8e1680abd77
Mitr3z <130659279+Mitr3z@users.noreply.github.com>
2024-07-04 18:51:56 -0300

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,21 @@
+const barberServicePrices = {
+    "Taverna Viking: Barbearia-Tattoo-Bar": {
+      "CORTES DE CABELO TRADICIONAIS": "R$ 50,00",
+      "BARBEAR": "R$ 30,00",
+      "CORTE E BARBA": "R$ 70,00",
+      "CORTE COM VISAGISMO": "R$ 80,00"
+    },
+    "Barbearia DiNavalha": {
+      "CORTES DE CABELO TRADICIONAIS": "R$ 60,00",
+      "BARBEAR": "R$ 35,00",
+      "CORTE E BARBA": "R$ 85,00",
+      "CORTE COM VISAGISMO": "R$ 90,00"
+    },
+    "Seu bigode barbershop": {
+      "CORTES DE CABELO TRADICIONAIS": "R$ 45,00",
+      "BARBEAR": "R$ 25,00",
+      "CORTE E BARBA": "R$ 65,00",
+      "CORTE COM VISAGISMO": "R$ 70,00"
+    }
+  };
+  
